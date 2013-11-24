ab856878fb1b2a7ba444431306c6b2f903ab1148
David Huie <david@nationbuilder.com>
2013-11-24 12:24:51 -0800

    Reorganizing stuff

--- beef_server/beef_server.go
+++ beef_server/beef_server.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
+// import zmq "github.com/DavidHuie/beef/vendor/go-zmq"
+
 // This file will parse command line arguments
 // and start a beef server.
 
 func main() {
-	fmt.Println("beef!")
+	fmt.Println("beef server!")
 }
--- server/server.go
+++
@@ -1,3 +0,0 @@
-package server
-
-import zmq "github.com/DavidHuie/beef/vendor/gozmq"
