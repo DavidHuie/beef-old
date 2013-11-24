7f93a7150ba247afcb7001ed836cc55e740018a6
David Huie <dahuie@gmail.com>
2013-11-23 20:25:20 -0800

    Fixed typo

--- string_hash/string_hash.go
+++ string_hash/string_hash.go
@@ -3,7 +3,7 @@ package string_hash
 import "hash/fnv"
 
 // Hash takes in a string to hash and an integer
-// that corresponds to which hash number to apply.
+// that corresponds to which hash function to apply.
 func Hash(str string, hash_number int) uint64 {
 	return uint64(0)
 }
