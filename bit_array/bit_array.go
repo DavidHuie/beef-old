5b3c768472f0a249a2ae0ee90a977cc24450dcb4
David Huie <dahuie@gmail.com>
2013-11-23 19:54:17 -0800

    Seemingly working bit array and test

--- bit_array/bit_array.go
+++ bit_array/bit_array.go
@@ -1,5 +1,6 @@
 package bit_array
 
+const bit uint64 = 1
 const bits uint64 = 64
 
 type BitArray struct {
@@ -7,25 +8,22 @@ type BitArray struct {
 	data []uint64
 }
 
-func expected_size(size uint64) uint64 {
-	// This is an estimate.
-	return (size / bits) + 1
-}
-
 // New takes in a size and outputs a pointer to a new BitArray.
 func New(size uint64) *BitArray {
 	ba := new(BitArray)
 	ba.size = size
-	ba.data = make([]uint64, expected_size(size))
+	ba.data = make([]uint64, (size/bits)+1)
 	return ba
 }
 
+// Sets the bit array bit at the input position to 1.
 func (b *BitArray) Set(position uint64) {
 	integer := position / b.size
-	bit := position % bits
-	b.data[integer] = b.data[integer] & (uint64(1) << bit)
+	bit_position := position % bits
+	b.data[integer] = b.data[integer] | (bit << bit_position)
 }
 
+// Returns the bit at the input position.
 func (b *BitArray) Get(position uint64) uint64 {
 	integer := position / b.size
 	bit := position % bits
--- bit_array/bit_array_test.go
+++ bit_array/bit_array_test.go
@@ -2,7 +2,54 @@ package bit_array
 
 import "testing"
 
+var test_new_bit_array_data = []struct {
+	array             *BitArray
+	expected_size     uint64
+	expected_data_len uint64
+}{
+	{New(0), 0, 1},
+	{New(1), 1, 1},
+	{New(63), 63, 1},
+	{New(64), 64, 2},
+	{New(129), 129, 3},
+}
+
 func TestNewBitArray(t *testing.T) {
-	// t.Errorf("Sqrt(%v) = %v, want %v", in, x, out)
-	ba := New(64)
+	for _, example := range test_new_bit_array_data {
+		if example.array.size != example.expected_size {
+			t.Errorf("Size of should be %v, got %v",
+				example.expected_size,
+				example.array.size)
+		}
+		if len(example.array.data) != int(example.expected_data_len) {
+			t.Errorf("Expected length %v, got %v",
+				example.expected_data_len,
+				example.array.data)
+		}
+		for _, value := range example.array.data {
+			if value != 0 {
+				t.Errorf("Data should be zeroed out")
+			}
+		}
+
+	}
+}
+
+func TestSet(t *testing.T) {
+	ba := New(70)
+	ba.Set(70)
+	if ba.data[1] != 64 {
+		t.Errorf("Integer set to %v, should be %v", ba.data[1], 1)
+	}
+}
+
+func TestGet(t *testing.T) {
+	ba := New(70)
+	if ba.Get(70) != uint64(0) {
+		t.Errorf("Entry at position %v should not be filled", 70)
+	}
+	ba.Set(70)
+	if ba.Get(70) != uint64(1) {
+		t.Errorf("Entry at position %v should be filled", 70)
+	}
 }
