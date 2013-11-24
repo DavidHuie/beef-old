97b96ccfe1f1ee3724ee269393c1b33f9afeee51
David Huie <dahuie@gmail.com>
2013-11-23 20:21:33 -0800

    A template for string_hash

---
+++ string_hash/string_hash.go
@@ -0,0 +1,9 @@
+package string_hash
+
+import "hash/fnv"
+
+// Hash takes in a string to hash and an integer
+// that corresponds to which hash number to apply.
+func Hash(str string, hash_number int) uint64 {
+	return uint64(0)
+}
