97e79545e553e8f7af79238f6020e92a18d8e3a1
David Huie <david@nationbuilder.com>
2013-12-11 19:21:30 -0800

    Error handling for managers

--- manager.go
+++ manager.go
@@ -1,5 +1,7 @@
 package beef
 
+import "errors"
+
 const (
 	// The size of an underlying bit array
 	// is this number times the expected size.
@@ -19,12 +21,20 @@ func NewManager() *Manager {
 	return bs
 }
 
-func (b *Manager) CreateBF(name string, size uint64) {
+func (b *Manager) CreateBF(name string, size uint64) error {
+	if _, ok := b.bit_arrays[name]; ok {
+		return errors.New("Bloom filter already exists")
+	}
 	b.bit_arrays[name] = NewBloomFilter(size*bit_array_factor, num_hashes)
+	return nil
 }
 
-func (b *Manager) DeleteBF(name string) {
+func (b *Manager) DeleteBF(name string) error {
+	if _, ok := b.bit_arrays[name]; !ok {
+		return errors.New("Bloom filter does not exist")
+	}
 	delete(b.bit_arrays, name)
+	return nil
 }
 
 func (b *Manager) ExistsBF(name string) bool {
@@ -32,10 +42,17 @@ func (b *Manager) ExistsBF(name string) bool {
 	return ok
 }
 
-func (b *Manager) InsertBF(name string, value string) {
+func (b *Manager) InsertBF(name string, value string) error {
+	if _, ok := b.bit_arrays[name]; !ok {
+		return errors.New("Bloom filter does not exist")
+	}
 	b.bit_arrays[name].Insert(value)
+	return nil
 }
 
-func (b *Manager) CheckBF(name string, value string) bool {
-	return b.bit_arrays[name].Check(value)
+func (b *Manager) CheckBF(name string, value string) (bool, error) {
+	if _, ok := b.bit_arrays[name]; !ok {
+		return false, errors.New("Bloom filter does not exist")
+	}
+	return b.bit_arrays[name].Check(value), nil
 }
--- manager_test.go
+++ manager_test.go
@@ -11,19 +11,39 @@ func TestAllFunctions(t *testing.T) {
 		t.Errorf("Test bloom filter should not exist")
 	}
 
-	server.CreateBF(bf_name, 1000)
+	error := server.CreateBF(bf_name, 1000)
+
+	if error != nil {
+		t.Errorf("Creating a bloom filter should not throw an error")
+	}
 
 	if !server.ExistsBF(bf_name) {
 		t.Errorf("Test bloom filter should exist")
 	}
 
-	if server.CheckBF(bf_name, "test value") {
+	if value, _ := server.CheckBF(bf_name, "test value"); value {
 		t.Errorf("Value should not be set")
 	}
 
-	server.InsertBF(bf_name, "test value")
+	if _, error := server.CheckBF(bf_name, "test value"); error != nil {
+		t.Errorf("There should not be errors because the bloom filter has been created")
+	}
 
-	if !server.CheckBF(bf_name, "test value") {
-		t.Errorf("Value should now be set")
+	if _, error := server.CheckBF("xxx", "test value"); error == nil {
+		t.Errorf("Checking for non-existing bloom filter should throw an error")
+	}
+
+	error = server.InsertBF(bf_name, "test value")
+
+	if error != nil {
+		t.Errorf("There should not be an error inserting into existing bloom filter")
+	}
+
+	if _, error := server.CheckBF(bf_name, "test value"); error != nil {
+		t.Errorf("Checking for a set value should not throw an error")
+	}
+
+	if value, _ := server.CheckBF(bf_name, "test value"); !value {
+		t.Errorf("Checking for a set value should not throw an error")
 	}
 }
